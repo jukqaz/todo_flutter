3c0aab4da3fa1982e450942fe7f08e8d4a3dc76e
jukqaz <jukqaz@gmail.com>
2023-03-09 02:36:17 +0900

    feat: Add task extension

--- lib/model/task.dart
+++ lib/model/task.dart
@@ -3,6 +3,8 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'task.freezed.dart';
 part 'task.g.dart';
 
+enum TaskStatus { normal, completed, deleted }
+
 @freezed
 class Task with _$Task {
   const factory Task({
@@ -16,4 +18,18 @@ class Task with _$Task {
   }) = _Task;
 
   factory Task.fromJson(Map<String, dynamic> json) => _$TaskFromJson(json);
+}
+
+extension on Task {
+  bool get isCompleted => status == TaskStatus.completed;
+  bool get isDeleted => status == TaskStatus.deleted;
+  TaskStatus get status {
+    if (isDeleted) {
+      return TaskStatus.deleted;
+    } else if (isCompleted) {
+      return TaskStatus.completed;
+    } else {
+      return TaskStatus.normal;
+    }
+  }
 }
