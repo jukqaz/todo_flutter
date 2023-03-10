7e6e9999e5a318b8acbbf20e9f40d4f34d07f216
myeongho.ham <myeongho.ham@looxidlabs.com>
2023-03-10 13:03:26 +0900

    feat: Set firebase firestore cache

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
@@ -12,6 +13,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  if (kIsWeb) {
+    FirebaseFirestore.instance.enablePersistence();
+  } else {
+    FirebaseFirestore.instance.settings = const Settings(cacheSizeBytes: 100 * 1024 * 1024);
+  }
 
   FlutterError.onError = (errorDetails) {
     FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
